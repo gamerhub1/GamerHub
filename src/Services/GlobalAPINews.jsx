1c3f7b0f75ec688a0a3871bb31089a742488b92c
GamerHub_Code <Gamerhub652@gmail.com>
2024-11-03 01:52:26 -0300

    Atualização Segurança  2.2

--- src/Services/GlobalAPINews.jsx
+++ src/Services/GlobalAPINews.jsx
@@ -3,6 +3,10 @@ import axios from "axios";
 const key = "f2d374e5416d45ec82f5454d25f4d37c";
 const axiosNewsInstance = axios.create({
   baseURL: 'https://newsapi.org/v2',
+  headers: {
+    'User-Agent': 'Mozilla/5.0',
+    'Accept': 'application/json'
+  }
 });
 
 
