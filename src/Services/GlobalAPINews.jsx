6c3d979d2ce914345a37121d5c178bdacf561689
GamerHub_Code <Gamerhub652@gmail.com>
2024-11-12 20:27:05 -0300

    correção newsapi

--- src/Services/GlobalAPINews.jsx
+++ src/Services/GlobalAPINews.jsx
@@ -1,19 +1,18 @@
-export default async function handler(req, res) {
-  const apiKey = process.env.NEWS_API_KEY; 
-  const query = req.query.q || "games";
-  const language = req.query.lang || "pt";
-  const sortBy = req.query.sortBy || "publishedAt";
+import axios from "axios";
 
-  const url = `https://newsapi.org/v2/everything?q=${query}&language=${language}&sortBy=${sortBy}&apiKey=${apiKey}`;
-
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      return res.status(response.status).json({ error: "Erro ao buscar dados da News API" });
-    }
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Erro interno no servidor", details: error.message });
+const key = "f2d374e5416d45ec82f5454d25f4d37c";
+const axiosNewsInstance = axios.create({
+  baseURL: 'https://newsapi.org/v2',
+  headers: {
+    'User-Agent': 'Mozilla/5.0',
+    'Accept': 'application/json'
   }
-}
+});
+
+
+const fetchNews = (query = "games") => 
+  axiosNewsInstance.get(`/everything?q=${query}&sortBy=publishedAt&apiKey=${key}`);
+
+export default {
+  fetchNews,
+};
