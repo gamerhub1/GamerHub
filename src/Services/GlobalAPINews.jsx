36d995ef832da6fb922964453c9706f242e2a7d6
GamerHub_Code <Gamerhub652@gmail.com>
2024-11-12 20:32:39 -0300

    correção newsapi

--- src/Services/GlobalAPINews.jsx
+++ src/Services/GlobalAPINews.jsx
@@ -1,18 +1,13 @@
-import axios from "axios";
-
-const key = "f2d374e5416d45ec82f5454d25f4d37c";
-const axiosNewsInstance = axios.create({
-  baseURL: 'https://newsapi.org/v2',
-  headers: {
-    'User-Agent': 'Mozilla/5.0',
-    'Accept': 'application/json'
+const fetchNews = async (query) => {
+  try {
+    const response = await axios.get(`https://newsapi.org/v2/everything?q=${query || "game"}&language=pt&apiKey=f2d374e5416d45ec82f5454d25f4d37c`);
+    console.log(response);
+    
+    if (response.data.articles.length === 0) {
+      console.log("Nenhuma notícia encontrada");
+    }
+    setNews(response.data.articles);
+  } catch (error) {
+    console.error("Erro ao buscar notícias:", error);
   }
-});
-
-
-const fetchNews = (query = "games") => 
-  axiosNewsInstance.get(`/everything?q=${query}&sortBy=publishedAt&apiKey=${key}`);
-
-export default {
-  fetchNews,
 };
