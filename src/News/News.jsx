c27f493214064754b6656b558ca8e44239fa6753
GamerHub_Code <Gamerhub652@gmail.com>
2024-11-12 23:07:11 -0300

    Atualização de Busca

--- src/News/News.jsx
+++ src/News/News.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../Services/GlobalAPINews'; 
 import './News.css';
 
 const News = () => {
   const [news, setNews] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [searchTerm, setSearchTerm] = useState(""); 
+  const [searchTerm, setSearchTerm] = useState("games");  // Inicia com "games" como padrão
   const newsPerPage = 6;
 
-
+  // Função para buscar as notícias com base no termo de pesquisa
   const fetchNews = async (query) => {
     try {
       const response = await api.fetchNews(query);  
@@ -18,12 +18,11 @@ const News = () => {
     }
   };
 
-
+  // Busca inicial com "games" quando o componente for montado
   useEffect(() => {
-    fetchNews(searchTerm || "games");  
-  }, [searchTerm]);
+    fetchNews(searchTerm); 
+  }, []); // Executa apenas uma vez ao carregar o componente
 
-  
   const indexOfLastNews = currentPage * newsPerPage;
   const indexOfFirstNews = indexOfLastNews - newsPerPage;
   const currentNews = news.slice(indexOfFirstNews, indexOfLastNews);
@@ -41,9 +40,17 @@ const News = () => {
     }
   };
 
+  // Atualiza o valor da pesquisa
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
-    setCurrentPage(1);  
+    setCurrentPage(1);  // Reseta para a página 1 ao mudar o texto
+  };
+
+  // Realiza a busca quando pressionar Enter
+  const handleSearchSubmit = (event) => {
+    if (event.key === "Enter") {
+      fetchNews(searchTerm); // Chama a API apenas quando Enter for pressionado
+    }
   };
 
   return (
@@ -53,6 +60,7 @@ const News = () => {
         placeholder="Pesquise um Jogo ou Empresa..."
         value={searchTerm}
         onChange={handleSearchChange}
+        onKeyDown={handleSearchSubmit}  // Dispara a pesquisa somente quando pressionar Enter
         className="search-barNews"
       />
 
